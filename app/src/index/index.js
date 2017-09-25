922a7cf0c3c45b56bf9c5d46564e57b8d21becd0
Rainfore <rainforest92@126.com>
2017-09-26 00:40:48 +0800

    :sparkles: Update app template

---
+++ app/src/index/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import Index from './index.vue';
+
+new Vue(Index).$mount('#app');
--- app/vusion.config.js
+++ app/vusion.config.js
@@ -1,12 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = global.plugins.HtmlWebpackPlugin;
+
 module.exports = {
+    version: '^0.5.0',
     type: 'app',
+    uglifyJS: true,
+    extractCSS: true,
     webpack: {
         entry: {
-            bundle: './src/index.js',
+            index: './src/index/index.js',
+            dashboard: './src/dashboard/index.js',
+            login: './src/login/index.js',
         },
+        plugins: [
+            'EXTENDS',
+            new HtmlWebpackPlugin({ filename: 'index.html', hash: true, template: './src/index/index.html', chunks: ['index'] }),
+            new HtmlWebpackPlugin({ filename: 'dashboard.html', hash: true, template: './src/dashboard/index.html', chunks: ['dashboard'] }),
+            new HtmlWebpackPlugin({ filename: 'login.html', hash: true, template: './src/login/index.html', chunks: ['login'] }),
+        ],
     },
-    webpackDevServer:{
-    	contentBase: __dirname,
-    	publicPath: "/public/"
-    }
-}
+};
