1aa1a97a3568df8c0aa14da526eb2fa6d2bf2731
xieqiang <18252063790@163.com>
2018-03-05 20:50:27 +0800

    fix fullstack-pro template

--- fullstack-pro/vukoa.config.js
+++ fullstack-pro/vukoa.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     type: 'app',
     root: __dirname,
-    port: 8001,
+    port: 8002,
     entry: './src/server/app',
     controller: './src/server/app/controller/',
     routes: './src/server/config/routes.js',
