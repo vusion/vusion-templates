93ff5137e51e1f0924f891fe0b067461b738b0e1
Rainfore <rainforest92@126.com>
2019-01-30 16:27:26 +0800

    :ambulance: Support i18n

--- fullstack-pro/src/client/views/login/index.js
+++ fullstack-pro/src/client/views/login/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 
 import * as Components from 'library';
-import { installComponents } from 'vusion-utils';
-installComponents(Components, Vue);
+import { install } from 'vusion-utils';
+install(Components, Vue);
 
 import Index from './index.vue';
 new Vue(Index).$mount('#app');
